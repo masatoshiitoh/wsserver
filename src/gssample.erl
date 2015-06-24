de4abaacd0333ff01268de466a40c616f77518e1
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-06-24 19:36:31 +0900

    Add gssample.erl as a gen_server sample.

---
+++ src/gssample.erl
@@ -0,0 +1,40 @@
+-module(gssample).
+-behaviour(gen_server).
+
+-export([add/2, sub/2]).
+
+-export([start_link/0]).
+-export([terminate/2]).
+-export([init/1]).
+-export([handle_call/3]).
+
+
+add(A,B) ->
+	Reply = gen_server:call(?MODULE, {add, A, B}),
+	{ok, V} = Reply,
+	V.
+
+sub(A,B) ->
+	Reply = gen_server:call(?MODULE, {sub, A, B}),
+	{ok, V} = Reply,
+	V.
+
+
+start_link() ->
+	Args = [],
+	gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+init(_Args) ->
+	NewState = [],
+	{ok, NewState}.
+
+terminate(_Reason, State) ->
+	ok.
+
+handle_call({add, A, B}, From, State) ->
+	{reply, {ok, A+B}, State};
+
+handle_call({sub, A, B}, From, State) ->
+	{reply, {ok, A-B}, State}.
+
+
