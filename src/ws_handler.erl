2e800f50a8c92f41ac11dcbdf7f0669ad0c1033f
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-07-06 22:13:08 +0900

    Prepare demonstration.

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -6,41 +6,26 @@
 -export([websocket_info/3]).
 -export([websocket_terminate/3]).
 
-%%-record(state, {count,stat}).
-
 init(Req, Opts) ->
-	State = [0],
-	{cowboy_websocket, Req, State}.
+	erlang:start_timer(1000, self(), <<"Hello!">>),
+	{cowboy_websocket, Req, Opts}.
 
-websocket_init(_, Req, State) ->
+websocket_init(_, Req, _Opts) ->
 	Req2 = cowboy_req:compact(Req),
-	{ok, Req2, State}.
+	{ok, Req2, []}.
 
 % get_listメッセージの場合はメッセージのリストを返します
 websocket_handle({text, <<"now">>}, Req, State) ->
-
-	[C] = State,
 	{{Year,Mon,Day},{Hour,Min,Sec}} = erlang:localtime(),
-	TimeStr = io_lib:format("~p/~p/~p ~p:~p:~p (stat= ~p)", [Year,Mon,Day,Hour,Min,Sec, C]),
-
+	TimeStr = io_lib:format("~p/~p/~p ~p:~p:~p", [Year,Mon,Day,Hour,Min,Sec]),
 	BinTimeStr = list_to_binary(TimeStr),
-
-	{reply, {text, BinTimeStr}, Req, [C+1]};
+	{reply, {text, BinTimeStr}, Req, State};
 
 websocket_handle({text, Data}, Req, State) ->
-
-	[C] = State,
-	T = io_lib:format("~p (count= ~p)", [ binary_to_list(Data), C]),
-	B = list_to_binary(T),
-
-	{reply, {text, B}, Req, [C+1]};
+	{reply, {text, Data}, Req, State};
 
 websocket_handle({binary, Data}, Req, State) ->
-
-	[C] = State,
-	T = io_lib:format("~p (count= ~p)", [ binary_to_list(Data), C]),
-	B = list_to_binary(T),
-	{reply, {binary, B}, Req, [C+1]};
+	{reply, {binary, Data}, Req, State};
 
 websocket_handle(_Frame, Req, State) ->
 	{ok, Req, State}.
